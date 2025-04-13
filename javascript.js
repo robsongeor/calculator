7af8f617398ce37ec828d55d6013fee9ea334334
George R <robsongeorge@hotmail.com>
2025-04-13 21:14:29 +1200

    Create all buttons and display.
    uses a array to store button objects

--- javascript.js
+++ javascript.js
@@ -1,4 +1,48 @@
 let calculatorVaribles = {value1: 0, value2: 5, operator: add};
+const buttons = [
+     {value: reset, display: "AC"},
+    {value: invert, display: "+/-"},
+     {value: percent, display: "%"},
+    {value: divide, display: "/"},
+     {value: 7, display: "7"},
+     {value: 8, display: "8"},
+     {value: 9, display: "9"},
+     {value: multiply, display: "*"},
+     {value: 4, display: "4"},
+     {value: 5, display: "5"},
+     {value: 6, display: "6"},
+     {value: subtract, display: "-"},
+     {value: 1, display: "1"},
+     {value: 2, display: "2"},
+     {value: 3, display: "3"},
+     {value: add, display: "+"},
+     {value: 0, display: "0"},
+     {value: decimal, display: "."},
+     {value: equals, display: "="},
+
+]
+
+const calculatorContainer = document.getElementById("calculator-container");
+const calculatorButtonContainer = calculatorContainer.querySelector(".inputs");
+
+buttons.forEach(button => {
+    // Check if button is a operator or value (for naming the class)
+    let divClassName = 
+        typeof button.value === 'function' ? button.value.name : `${button.value}`;
+    const buttonElement = document.createElement("div");
+    buttonElement.classList.add(divClassName, "calc-button")
+    buttonElement.textContent = button.display;
+
+    calculatorButtonContainer.append(buttonElement);
+});
+
+
+
+function reset(){}
+function percent(){}
+function invert(){}
+function decimal(){}
+function equals(){}
 
 function add(val1, val2){
     return val1 + val2;
